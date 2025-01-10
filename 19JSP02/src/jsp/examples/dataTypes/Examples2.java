943b6610a1d8020cb2ead7b754eab5de7b613e61
kulkul <duyloc89dn@gmail.com>
2025-01-10 10:25:59 +0700

    Update Examples2.java
    gf123

--- 19JSP02/src/jsp/examples/dataTypes/Examples2.java
+++ 19JSP02/src/jsp/examples/dataTypes/Examples2.java
@@ -18,7 +18,7 @@ public class Examples2 {
 		// TODO Auto-generated method stub
 		Scanner input1 = new Scanner(System.in);
 		System.out.println("Please enter your name : ");
-		String fullName =  inpu123t1.nextLine();
+		String fullName =  input1.nextLine();
 		System.out.println("Your fullNam123e i11s : " + fullName);
 		
 		Scanner input2 = new Sc321anner(System.in);
