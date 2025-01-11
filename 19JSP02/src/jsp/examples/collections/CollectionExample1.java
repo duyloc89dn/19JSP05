ab6a20229a8cd5fdc756b0d2b88daa3a391aede3
kulkul <duyloc89dn@gmail.com>
2025-01-11 09:11:13 +0700

    Update CollectionExample1.java
    remove1123

--- 19JSP02/src/jsp/examples/collections/CollectionExample1.java
+++ 19JSP02/src/jsp/examples/collections/CollectionExample1.java
@@ -19,7 +19,7 @@ public class CollectionExample1 {
 	public static void main(String[] args) {
         List<String> arrayList = new ArrayList<String>();
         arrayList.add("PHP111");
-        arrayList.add("Pyt143hon1111");
+        arrayList.add("Python");
         arrayList.add("Java1");
         arrayList.add("C++123");
         System.out.println("Các phần tử của ArrayList");
