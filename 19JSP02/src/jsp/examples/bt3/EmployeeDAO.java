a3b37daf9ac3dc23a6914062c329ad8e173d9286
kulkul <duyloc89dn@gmail.com>
2025-01-15 10:17:26 +0700

    Update EmployeeDAO.java
    f2

--- 19JSP02/src/jsp/examples/bt3/EmployeeDAO.java
+++ 19JSP02/src/jsp/examples/bt3/EmployeeDAO.java
@@ -4,6 +4,6 @@ public interface EmployeeDAO {
 	//this is add function
 	public void addEmployee(Employee[] employees);
 	public void showEmployee(Employee[] employees);
-	public Employee searchByName(String nameInput, Employee[] employees);
+	public Employee sea12rchByName(String nameInput, Employee[] employees);
 	public void updateEmployeeByName(String nameInput, Double newSalary, Employee[] employees);
 }
