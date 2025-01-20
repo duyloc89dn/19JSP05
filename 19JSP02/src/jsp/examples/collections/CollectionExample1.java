717e8a94297f83644c43141250667075d8b00ab4
kulkul <duyloc89dn@gmail.com>
2025-01-20 15:59:05 +0700

    Update CollectionExample1.java
    fd21

--- 19JSP02/src/jsp/examples/collections/CollectionExample1.java
+++ 19JSP02/src/jsp/examples/collections/CollectionExample1.java
@@ -19,7 +19,7 @@ public class CollectionExample1 {
 	public static void main(String[] args) {
         List<String> arrayList = new ArrayList<String>();
         arrayList.add("PHP");
-        arrayList.add("Python");
+        arrayList.add("Python123");
         arrayList.add("Java1");
         arrayList.add("C++");
         System.out.println("Các phần tử của ArrayList");
